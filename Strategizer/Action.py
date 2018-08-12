48c1239b99b33738879fa9d75e39617bf5e60b8f
Patrick Weber <patrick310@gmail.com>
2018-08-11 22:52:40 -0500

    Expanded model and added justification

--- Strategizer/Action.py
+++ Strategizer/Action.py
@@ -3,5 +3,9 @@ class Action:
     Represents a possible move towards completion of a goal
     """
 
-    def __init__(self, goal):
+    def __init__(self, goal, date, autonomy_rating=None, cue_strength_rating=None):
         self.goal = goal
+        self.date = date
+
+        self.autonomy_rating = autonomy_rating
+        self.cue_strength_rating = cue_strength_rating
